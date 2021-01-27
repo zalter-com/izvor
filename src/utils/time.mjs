d830a74dc6e704f6812b86e4888cbd755fa30f1f
Alexandru Robert <comanescu.robert93@gmail.com>
2021-01-27 17:46:30 +0200

    Remove unused util

--- src/utils/time.mjs
+++
@@ -1,9 +0,0 @@
-/**
- * Adds days to a given date
- * @param {Date} date
- * @param {number} daysCount
- * @return {Date}
- */
-export const addDays = (date, daysCount) => {
-	return new Date(date.getTime() + daysCount * 8.64e7);
-};
